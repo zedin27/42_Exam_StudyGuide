32f84c5c38da92e66258aa3e974d5a60c1e00488
zedin27 <ztisneshamzeh@csumb.edu>
2018-05-13 10:13:56 -0700

    FIX: added proper solution & corrected myself

--- lvl2/swap_bits_01/swap_bits.c
+++ lvl2/swap_bits_01/swap_bits.c
@@ -1,4 +1,9 @@
+/* FAIL - BOOLEAN != BIT */
+
+//Good 40mins to figure my problem lmao
 unsigned char	swap_bits(unsigned char octet)
 {
 	return ((octet >> 4 || octet << 4));
+
+	//return ((octet >> 4 | octet << 4)); <---- RIGHT
 }
