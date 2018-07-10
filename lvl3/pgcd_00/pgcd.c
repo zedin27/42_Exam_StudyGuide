6c8a5f50d471f23c87b762347b387b102872300a
zedin27 <ztisneshamzeh@csumb.edu>
2018-07-09 21:59:46 -0700

    started GCD

---
+++ lvl3/pgcd_00/pgcd.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/*
+** Greatest Common Divisor OR Greatest Common Factor calculator
+*/
+
+int		main(int argc, char *argv[])
+{
+	if (argc == 3)
+		//function function
+
+}
