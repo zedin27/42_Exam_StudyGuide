65ffdbf96c7992070ec16c0385c0ef1d864cc108
zedin27 <zeidtisnes@gmail.com>
2018-07-17 19:44:47 -0700

    level 3 pgcd added

--- lvl3/pgcd_00/pgcd.c
+++ lvl3/pgcd_00/pgcd.c
@@ -1,13 +1,34 @@
 #include <unistd.h>
-#include <stdio.h>
+#include <stdio.h> //printf function
+#include <stdlib.h> //atoi function
 
 /*
 ** Greatest Common Divisor OR Greatest Common Factor calculator
+** Attempt the code in 3 different ways:
+** -> mod ✅
+** -> substraction
+** -> recursive
 */
+
+int		gcd(int num, int den)
+{
+	int tmp;
+
+	while (den != 0)
+	{
+		tmp = den;
+		den = (num % den);
+		num = tmp;
+	}
+	if (num < 0)
+		num *= -1;
+	return (num);
+}
 
 int		main(int argc, char *argv[])
 {
 	if (argc == 3)
-		//function function
-
+		printf("GCD for %s and %s is %d", argv[1], argv[2], gcd(atoi(argv[1]), atoi(argv[2])));
+	printf("\n");
+	return (0);
 }
