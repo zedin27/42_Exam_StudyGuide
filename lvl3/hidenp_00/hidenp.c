46c393d63cec8c2f400d564819cfeb32438dc1e0
zedin27 <zeidtisnes@gmail.com>
2018-07-20 10:43:15 -0700

    new hidenp approach

--- lvl3/hidenp_00/hidenp.c
+++ lvl3/hidenp_00/hidenp.c
@@ -1,5 +1,8 @@
 #include <unistd.h>
 #include <stdio.h>
+
+//FAIL
+
 /*
 ** find s1 (secret string) from s2 (source string)
 */
@@ -11,7 +14,7 @@ int			ft_strcmp(const char *s1, const char *s2)
 
 	i = 0;
 	j = 0;
-	while (s1[i] == s2[j] && s2[j])
+	while (s2[j] && (s1[i] == s2[j]))
 	{
 		i++;
 		j++;
@@ -31,13 +34,12 @@ int		hidenp(char *s1, char *s2)
 	{
 		if (s1[j] == s2[i])
 		{
-			//printf("%c", s1[j]);
 			found[j] = s1[j];
 			j++;
 		}
 		i++;
 	}
-	if (!(ft_strcmp(s2, found)))
+	if (!(ft_strcmp(s1, found)))
 		write(1, "1", 1);
 	else
 		write(1, "0", 1);
@@ -52,13 +54,5 @@ int		main(int argc, char *argv[])
 	char *str1 = "abc";
 	char *str2 = "halbc";
 	hidenp(str1, str2);
-	// int i;
-	//
-	// i = 0;
-	// if (argc != 2)
-	// {
-	// 	write(1, "\n", 1);
-	// 	return (0);
-	// }
-	// while ()
+	printf("\n");
 }
---
+++ lvl3/hidenp_01/hidenp.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void	hidenp(char *s1, char *s2)
+{
+	while (*s2)
+	{
+		if (*s1 == *s2++)
+			s1++;
+	}
+	(*s1 == '\0') ? write(1, "1", 1) : write(1, "0", 1);
+}
+
+int		main(int argc, char *argv[])
+{
+	if (argc == 3)
+		hidenp(argv[1], argv[2]);
+	write(1, "\n", 1);
+	return (0);
+}
