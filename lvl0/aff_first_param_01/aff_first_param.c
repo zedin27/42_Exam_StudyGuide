c3d4f1a46d30810f54524c541a8034b2b65e4dc7
zedin27 <zeidtisnes@gmail.com>
2018-07-22 12:53:15 -0700

    another try of aff first param

---
+++ lvl0/aff_first_param_01/aff_first_param.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc >= 2)
+		ft_putstr(argv[1]);
+	else if (argc < 1)
+	{
+		ft_putstr("\n");
+		return (0);
+	}
+	ft_putstr("\n");
+	return (0);
+}
