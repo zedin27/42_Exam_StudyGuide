48ff9e27a52ab31ac43de1575aa87cded2ac5fb7
zedin27 <ztisneshamzeh@csumb.edu>
2019-08-16 21:19:14 -0700

    added camel_to_snake

---
+++ lvl2/camel_to_snake_00/camel_to_snake.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+
+int		ft_tolower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	return (c);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		write(1, &str[i++], 1);
+}
+
+void	camel_to_snake(char *str)
+{
+	int i;
+	i = -1;
+
+	while (str[++i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			ft_putchar('_');
+			ft_putchar(ft_tolower(str[i]));
+			continue ;
+		}
+		ft_putchar(str[i]);
+	}
+	return ;
+}
+
+int		main(int argc, char *argv[])
+{
+	if (argc == 2)
+		camel_to_snake(argv[1]);
+	ft_putchar('\n');
+	return (0);
+}
