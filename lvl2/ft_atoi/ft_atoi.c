0de0bc20c528cad13b641c7c5b5e3b4ff6f97d4e
zedin27 <ztisneshamzeh@csumb.edu>
2019-09-25 22:12:08 -0700

    fixed my ft_atoi

---
+++ lvl2/ft_atoi/ft_atoi.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+//Needs to be tested first
+
+int		ft_isspace(char c)
+{
+	if (c == ' ' || c == '\t')
+		return (1);
+	return (0);
+}
+
+int		ft_atoi(const char *str)
+{
+	int sign;
+	int num;
+	int res;
+	int i;
+
+	sign = 1;
+	res = 0;
+	i = 0;
+
+	if (str[i] == '\0')
+		return (0);
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res *= 10;
+		num = str[i] - '0';
+		res += num;
+		i++;
+	}
+	return (res * sign);
+}
