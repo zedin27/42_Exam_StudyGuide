763b4c7d2efb8288a62f89f50bd755b8b09d906a
zedin27 <ztisneshamzeh@csumb.edu>
2019-11-04 17:17:37 -0800

    no need strlen

--- lvl2/inter/inter.c
+++ lvl2/inter/inter.c
@@ -1,46 +1,36 @@
 #include <unistd.h>
 
-int    ft_strlen(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
-}
-
 void    inter(char *str1, char *str2)
 {
-    int i;
-    int ascii[256] = {0};
+	int i;
+	int ascii[256] = {0};
 
-    i = 0;
-    while (str2[i])
-    {
-        if (ascii[(int)str2[i]] == 0)
-            ascii[(int)str2[i]] = 1;
-        i++;
-    }
+	i = 0;
+	while (str2[i])
+	{
+		if (ascii[(int)str2[i]] == 0)
+			ascii[(int)str2[i]] = 1;
+		i++;
+	}
 
-    i = 0;
-    while (str1[i])
-    {
-        if (ascii[(int)str1[i]] == 1)
-        {
-            ascii[(int)str1[i]] = 2;
-            write(1, &str1[i], 1);
-        }
-        i++;
-    }
+	i = 0;
+	while (str1[i])
+	{
+		if (ascii[(int)str1[i]] == 1)
+		{
+			ascii[(int)str1[i]] = 2;
+			write(1, &str1[i], 1);
+		}
+	i++;
+	}
 }
 
 #ifdef TEST
 int    main(int argc, char *argv[])
 {
-    if (argc == 3)
-        inter(argv[1], argv[2]);
-    write(1, "\n", 1);
-    return (0);
+	if (argc == 3)
+		inter(argv[1], argv[2]);
+	write(1, "\n", 1);
+	return (0);
 }
 #endif
