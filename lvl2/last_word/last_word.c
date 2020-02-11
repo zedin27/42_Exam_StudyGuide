68ce484139b84f281f48aff0fad71a883f7a7291
zedin27 <ztisneshamzeh@csumb.edu>
2020-02-10 19:22:53 -0800

    plz:

--- lvl2/last_word/last_word.c
+++ lvl2/last_word/last_word.c
@@ -2,41 +2,41 @@
 
 int ft_isspace(char c)
 {
-    if (c == ' ' || c == '\t')
-        return (1);
-    return (0);
+	if (c == ' ' || c == '\t')
+		return (1);
+	return (0);
 }
 
 void    ft_putstr(char *str)
 {
-    while (*str && (!(ft_isspace(*str))))
-        write(1, str++, 1);
+	while (*str && (!(ft_isspace(*str))))
+		write(1, str++, 1);
 }
 
 void    last_word(char *str)
 {
-    char *last;
-    int i;
+	char *last;
+	int i;
 
-    i = 0;
-    last = &str[i];
-    while (str[i])
-    {
-        if (ft_isspace(str[i]))
-        {
-            if (str[i + 1] >= '!' && str[i + 1] <= '~')
-                last = &str[i + 1];
-        }
-        i++;
-    }
-    if (last)
-        ft_putstr(last);
+	i = 0;
+	last = &str[i];
+	while (str[i])
+	{
+		if (ft_isspace(str[i]))
+		{
+			if (str[i + 1] >= '!' && str[i + 1] <= '~')
+				last = &str[i + 1];
+		}
+		i++;
+	}
+	if (last)
+		ft_putstr(last);
 }
 
 int     main(int argc, char *argv[])
 {
-    if (argc == 2)
-       last_word(argv[1]);
-    ft_putstr("\n");
-    return (0);
+	if (argc == 2)
+		last_word(argv[1]);
+	ft_putstr("\n");
+	return (0);
 }
