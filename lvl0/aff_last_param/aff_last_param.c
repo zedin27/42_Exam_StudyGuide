1abdbe479044764cc3cf757482f44dba6e07befd
zedin27 <ztisneshamzeh@csumb.edu>
2020-02-12 15:46:11 -0800

    aff_last_param wasn't consistent

--- lvl0/aff_last_param/aff_last_param.c
+++ lvl0/aff_last_param/aff_last_param.c
@@ -2,14 +2,14 @@
 
 void    ft_putstr(char *str)
 {
-    while (*str)
-        write(1, str++, 1);
+	while (*str)
+		write(1, str++, 1);
 }
 
-int     main(int argc, char *argv[])
+int		main(int argc, char *argv[])
 {
-    if (argc > 1)
-        ft_putstr(argv[argc - 1]);
-    write(1, "\n", 1);
-    return (0);
+	if (argc > 1)
+		ft_putstr(argv[argc - 1]);
+	write(1, "\n", 1);
+	return (0);
 }
