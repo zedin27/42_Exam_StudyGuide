33f3961f58c0690a41a9d0b56ecbf392c5bb554a
zedin27 <ztisneshamzeh@csumb.edu>
2020-02-18 08:37:05 -0800

    plz

--- lvl3/pgcd/pgcd.c
+++ lvl3/pgcd/pgcd.c
@@ -6,30 +6,30 @@
 ** Recursive method
 */
 
-int     pgcd(int num, int den)
+int		pgcd(int num, int den)
 {
-    if (den == 0)
-        return (num);
-    else
-        return (pgcd(den, num % den));
+	if (den == 0)
+		return (num);
+	else
+		return (pgcd(den, num % den));
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc >= 2)
-    {
-        printf("%d\n", pgcd((atoi(argv[1])), atoi(argv[2])));
-        return (0);
-    }
-    write(1, "\n", 1);
-    return (0);
-    /*
+	if (argc >= 2)
+	{
+		printf("%d\n", pgcd((atoi(argv[1])), atoi(argv[2])));
+			return (0);
+	}
+	write(1, "\n", 1);
+	return (0);
+	/*
 	if (argc != 4)
-    {
-        write(1, "\n",x 1);
-        return (0);
-    }
-    printf("%d\n", pgcd((atoi(argv[2])), atoi(argv[3])));
-    // printf("%d\n", pgcd(80, 100));
+	{
+		write(1, "\n",x 1);
+		return (0);
+	}
+	printf("%d\n", pgcd((atoi(argv[2])), atoi(argv[3])));
+	// printf("%d\n", pgcd(80, 100));
 	*/
 }
