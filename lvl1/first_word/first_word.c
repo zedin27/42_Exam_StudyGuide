cb95bb49baf40893fedc285c90da640ac10e445b
zedin27 <ztisneshamzeh@csumb.edu>
2020-02-25 00:04:53 -0800

    consistency with my tabs

--- lvl1/first_word/first_word.c
+++ lvl1/first_word/first_word.c
@@ -2,31 +2,31 @@
 
 void    ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
 int     ft_isspace(char c)
 {
-    if (c == ' ' || c == '\t')
-        return (1);
-    return (0);
+	if (c == ' ' || c == '\t')
+		return (1);
+		return (0);
 }
 
 void    first_word(char *str)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (str[i] && ft_isspace(str[i]))
-        i++;
-    while (str[i] && !(ft_isspace(str[i])))
-        ft_putchar(str[i++]);
+	i = 0;
+	while (str[i] && ft_isspace(str[i]))
+		i++;
+	while (str[i] && !(ft_isspace(str[i])))
+		ft_putchar(str[i++]);
 }
 
 int     main(int argc, char *argv[])
 {
-    if (argc == 2)
-        first_word(argv[1]);
-    ft_putchar('\n');
-    return (0);
+	if (argc == 2)
+		first_word(argv[1]);
+	ft_putchar('\n');
+	return (0);
 }
