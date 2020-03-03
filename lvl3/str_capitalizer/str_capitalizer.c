9e0630be0ff0ab3501dde1fb48936216aaf2ea84
zedin27 <ztisneshamzeh@csumb.edu>
2020-03-03 03:00:14 -0800

    fixed str_capitalizer

--- lvl3/str_capitalizer/str_capitalizer.c
+++ lvl3/str_capitalizer/str_capitalizer.c
@@ -43,23 +43,26 @@ void	str_capitalizer(char *str)
 	while (str[i])
 	{
 		while (str[i] && ft_isspace(str[i]))
-			write(1, &str[i++], 1);
-		if (ft_islower(str[i]))
 		{
-			str[i] -= ('a' - 'A');
 			write(1, &str[i], 1);
 			i++;
 		}
-		while (str[i] && ft_isspace(str[i]))
+		if (str[i])
 		{
-			if (ft_isupper(str[i]))
-			{
-				str[i] += ('a' - 'A');
-				write(1, &str[i], 1);
-				i++;
-			}
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+			write(1, &str[i], 1);
+			i++;
+		}
+		while (str[i] && !ft_isspace(str[i]))
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] += 32;
+			write(1, &str[i], 1);
+			i++;
 		}
 	}
+	write(1, &str[i], 1);
 }
 
 int		main(int argc, char *argv[])
@@ -67,14 +70,16 @@ int		main(int argc, char *argv[])
 	int i;
 
 	i = 1;
-	if (argc >= 2)
+	if (argc == 1)
+		write(1, "\n", 1);
+	else
 	{
 		while (i < argc)
 		{
-			str_capitalizer(argv[i]);
+			if (argc > 1)
+				str_capitalizer(argv[i]);
 			i++;
 		}
 	}
-	write(1, "\n", 1);
 	return (0);
 }
